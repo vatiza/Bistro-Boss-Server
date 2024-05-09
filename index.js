fd158bd61ba2bfc468e5e256d4ae8b05f0ce1680
vatiza <rakibur.mac@gmail.com>
2024-05-09 07:44:43 +0600

    reviews

--- index.js
+++ index.js
@@ -23,10 +23,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menuCollection = client.db("bistroBossDB").collection("menu");
+    const reviewsCollection = client.db("bistroBossDB").collection("reviews");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const reviews = await reviewsCollection.find().toArray();
+      res.send(reviews);
     });
 
     // Send a ping to confirm a successful connection
